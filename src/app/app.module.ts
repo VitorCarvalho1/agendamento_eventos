6bf4573e46db411df6f4ba4bc9bbabbea5f55bce
vitor@vitor-Inspiron-3442 <vitor@vitor-Inspiron-3442>
2022-11-11 10:18:58 -0300

    header-Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRountingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
