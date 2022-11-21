dda95206bce781a1e397a72d6ae061d45b963a1a
vitor@vitor-Inspiron-3442 <vitor@vitor-Inspiron-3442>
2022-11-21 15:25:33 -0300

    att styles.css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  agendamentos = [
+    {
+      "status": "Em andamento",
+      "title": "FETECAgro 2022 2sementre",
+      "data": "16/11/2022",
+      "horario_inicio": "8h00",
+      "horario_fim": "22h00",
+      "endereco": "FATEC Pompéia"
+    },
+    {
+      "status": "Em andamento",
+      "title": "FETECAgro 2022 2sementre",
+      "data": "16/11/2022",
+      "horario_inicio": "8h00",
+      "horario_fim": "22h00",
+      "endereco": "FATEC Pompéia"
+    },
+    {
+      "status": "Em andamento",
+      "title": "FETECAgro 2022 2sementre",
+      "data": "16/11/2022",
+      "horario_inicio": "8h00",
+      "horario_fim": "22h00",
+      "endereco": "FATEC Pompéia"
+    },{
+      "status": "Em andamento",
+      "title": "FETECAgro 2022 2sementre",
+      "data": "16/11/2022",
+      "horario_inicio": "8h00",
+      "horario_fim": "22h00",
+      "endereco": "FATEC Pompéia"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
