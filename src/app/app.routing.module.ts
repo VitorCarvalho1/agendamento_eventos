ac2314a10727eb7e681d5c686ff5286901b32123
vitor@vitor-Inspiron-3442 <vitor@vitor-Inspiron-3442>
2022-11-28 11:05:03 -0300

    component tabela-adm done

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from "./home/home.component";
 const routes: Routes = [
     {path: '',component: HomeComponent},
     {path: 'tabela-adm',component: TabelaAdmComponent}
+    
 ]
 
 
